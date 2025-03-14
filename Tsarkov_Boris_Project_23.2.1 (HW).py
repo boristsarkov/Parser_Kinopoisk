f14ca3609ae08fad37e4abe2c7eedba979d2f655
Boris <borman-dallas@yandex.ru>
2025-03-14 11:12:51 +0400

    Update file Tsarkov_Boris_Project_23.2.1 (HW).py

---
+++ Tsarkov_Boris_Project_23.2.1 (HW).py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+user_login = '2576697'
+url = f'https://www.kinopoisk.ru/user/{user_login}/votes/'
+
+r = requests.get(url)
+
+with open('Kinopoisk', 'w', encoding='utf-8') as output_file:
+   output_file.write(r.text)
+
+
+soup = BeautifulSoup(r.text, 'lxml')
+entries = soup.find_all('div', class_='item')
+
+print(len(entries))
+data = []
+for entry in entries:
+   # В каждом элементе <tr> ищем элемент <td> с классом 'td-film-details'
+   td_film_details = entry.find('div', class_='info')
+   film_name = td_film_details.find('a').text
+   data.append({'film_name': film_name})
+
+print(data)
