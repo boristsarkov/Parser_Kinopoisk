1d69ca6525387ec90f5be38ac4785cb538ace155
Boris <borman-dallas@yandex.ru>
2025-03-17 09:31:49 +0400

    Update file Tsarkov_Boris_Project_23.2.1 (HW).py

--- Tsarkov_Boris_Project_23.2.1 (HW).py
+++ Tsarkov_Boris_Project_23.2.1 (HW).py
@@ -1,13 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+def get_online_content(type_content, page_num):
+    data = []
+    url = f'https://www.kinoafisha.info/online/{type_content}/?page={page_num}'
+    r = requests.get(url).text
+    soup = BeautifulSoup(r, 'lxml')
+    entries = soup.find_all('div', class_='movieList_item movieItem movieItem-grid grid_cell3')
+    for entry in entries:
+        div_film_details = entry.find('div', class_='movieItem_info')
+        film_name = div_film_details.find('a').text
+        mark = div_film_details.find('span', class_='mark_num').text
+        genre = div_film_details.find('span', class_='movieItem_genres').text
+        year_country = div_film_details.find('span', class_='movieItem_year').text
+        year = year_country.split(' ')
+        data.append({'film_name': film_name, 'mark': mark, 'genre': genre, 'year': year[0].replace(',', ''), 'country': year[1]})
 
-user_login = 2576697
-page_num = 1
-url = f'https://www.kinopoisk.ru/user/{user_login}/votes/list/vs/vote/page/{page_num}/#list'
-html_content = requests.get(url).text
+    return data
 
-soup = BeautifulSoup(html_content, 'lxml')
+#print(get_online_content('movies', 2))
+online_content = get_online_content('movies', 15)
+df = pd.DataFrame(online_content)
+df.to_excel('online_content.xlsx')
 
-entries = soup.find_all('div', class_='item')
-
-print(len(entries))
+"""n = '2004, Россия'
+d = []
+result = n.split(' ')
+year = result[0].replace(',', '')
+#year.replace(',', '')
+d.append({'year': year, 'coutry': result[1]})
+print(year.replace(',', ''))
+print(d)"""
